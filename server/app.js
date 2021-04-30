1fe129c8aa67d2c8db9c1fe15d1c41b3e92d36ea
ahuanggg <ah6595@rit.edu>
2021-04-29 21:41:31 -0400

    commented out a line

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const url = require('url');
-const redis = require('redis');
+// const RedisStore = require('connect-redis')(session);
+// const url = require('url');
+// const redis = require('redis');
 const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
@@ -17,35 +17,35 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = 'mongodb+srv://ahuanggg:Andyxie130@cluster0.styc6.mongodb.net/ChatRoom?authSource=admin&replicaSet=atlas-143hk9-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 
 const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
 };
 
 mongoose.connect(dbURL, mongooseOptions, (err) => {
-  if (err) {
-    console.log('Could not connect to database');
-    throw err;
-  }
+	if (err) {
+		console.log('Could not connect to database');
+		throw err;
+	}
 });
 
-let redisURL = {
-  hostname: 'redis-18952.c257.us-east-1-3.ec2.cloud.redislabs.com',
-  port: 18952,
-};
+// let redisURL = {
+//   hostname: 'redis-18952.c257.us-east-1-3.ec2.cloud.redislabs.com',
+//   port: 18952,
+// };
 
-let redisPASS = 'SbOIGnu4Xfoxj36YrjjoomgKNiUbsO0A';
+// let redisPASS = 'SbOIGnu4Xfoxj36YrjjoomgKNiUbsO0A';
 
-if (process.env.REDISCLOUD_URL) {
-  redisURL = url.parse(process.env.REDISCLOUD_URL);
-  [, redisPASS] = redisURL.auth.split(':');
-}
+// if (process.env.REDISCLOUD_URL) {
+//   redisURL = url.parse(process.env.REDISCLOUD_URL);
+//   [, redisPASS] = redisURL.auth.split(':');
+// }
 
-const redisClient = redis.createClient({
-  host: redisURL.hostname,
-  port: redisURL.port,
-  password: redisPASS,
-});
+// const redisClient = redis.createClient({
+//   host: redisURL.hostname,
+//   port: redisURL.port,
+//   password: redisPASS,
+// });
 
 const router = require('./router.js');
 
@@ -54,25 +54,25 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  session({
-    key: 'sessionid',
-    store: new RedisStore({
-      client: redisClient,
-    }),
-    secret: 'chatroom1',
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      httpOnly: true,
-    },
-  }),
-);
+// app.use(
+//   session({
+//     key: 'sessionid',
+//     store: new RedisStore({
+//       client: redisClient,
+//     }),
+//     secret: 'chatroom1',
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: {
+//       httpOnly: true,
+//     },
+//   }),
+// );
 app.engine(
-  'handlebars',
-  expressHandlebars({
-    defaultLayout: 'main',
-  }),
+	'handlebars',
+	expressHandlebars({
+		defaultLayout: 'main',
+	})
 );
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
@@ -82,17 +82,17 @@ app.use(cookieParser());
 
 app.use(csrf());
 app.use((err, req, res, next) => {
-  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+	if (err.code !== 'EBADCSRFTOKEN') return next(err);
 
-  // console.log('Missing CSRF token');
-  return false;
+	// console.log('Missing CSRF token');
+	return false;
 });
 
 router(app);
 
 app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Listening on port ${port}`);
+	if (err) {
+		throw err;
+	}
+	console.log(`Listening on port ${port}`);
 });
